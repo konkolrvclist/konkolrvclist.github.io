ace18060e7ee3ab2212672802d3da8151fbd9fb9
Chuck Konkol <ckonkol@gmail.com>
2020-01-11 15:15:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function get(name){
 function passlist()
 {
    var getshorturl=0;
-   var login = "o_3iokgmm945";
-   var api_key = "R_f2f3c9387a374e3fc6bf4b1ec2c945c4";
+   var login = "rvcjscourse";
+   var api_key = "7472f1a6e829e3450d98eaaa534f4ceb988e4311";
    var long_url = "https://rvclist.github.io/index.html?list="+ shoppinglist;
   try{
   $.getJSON(
