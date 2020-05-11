a935787fb92d8615810222069653dfff8cb3e91c
Chuck Konkol <ckonkol@gmail.com>
2020-05-11 01:35:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,10 +65,18 @@ function share()
 }
 //v4.1 prompt message to copy URL
 function copyToClipboard(text) {
+  var range, selection;
   const passbyurl = document.createElement('textarea');
   passbyurl.value = text;
   document.body.appendChild(passbyurl);
-  passbyurl.select();
+	//
+      range = document.createRange();
+      range.selectNodeContents(textArea);
+      selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      passbyurl.setSelectionRange(0, 999999);
+  //passbyurl.select();
   document.execCommand('copy');
   document.body.removeChild(passbyurl);
   alert("URL has been copied. Ready to share: " + text);
